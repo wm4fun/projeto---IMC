1a7803758f56651e47515c2d4b7223545267b472
William Milanez <william_milanez@hotmail.com>
2024-10-17 19:58:45 -0300

    iniciando o js com evento de submit e 3 maneiras de atribuir uma função a um evento

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+// variáveis - variables
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+// 3 maneiras de criar e atribuir função a um evento
+// 1ª
+// form.onsubmit = function(event) {
+//     event.preventDefault() // evite o padrão de enviar o formulário e recarregar a página
+
+//     const weight = inputWeight.value
+//     const height = inputHeight.value
+
+//     console.log(weight, height)
+// }
+
+// 2ª - arrow function
+form.onsubmit = event => {
+    event.preventDefault() // evite o padrão de enviar o formulário e recarregar a página
+    
+    const weight = inputWeight.value
+    const height = inputHeight.value
+
+    console.log(weight, height)
+}
+
+// 3ª
+// form.onsubmit = handleSubmit
+// function handleSubmit(event) {
+//     event.preventDefault() // evite o padrão de enviar o formulário e recarregar a página
+    
+//     const weight = inputWeight.value
+//     const height = inputHeight.value
+
+//     console.log(weight, height)
+// }
+
 
 
 
