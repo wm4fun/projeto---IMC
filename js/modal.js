a2dcba3be16d76f7be7ea74a22239871138562f2
William Milanez <william_milanez@hotmail.com>
2024-10-23 19:46:17 -0300

    fechando modal pelo teclado, com a tecla 'esc'

--- js/modal.js
+++ js/modal.js
@@ -15,4 +15,19 @@ Modal.buttonClose.onclick = () => {
     Modal.close()
 }
 
+// método alternativo, porém neste método só é aceito um por arquivo, e sempre pega o último "onkeydown"
+// window.onkeydown = handleKeydown
+// function handleKeydown(event) {
+// }
+
+// método com addEventListener, pode se repetir utilizando outros registros de "keydown"
+window.addEventListener('keydown', handleKeydown)
+function handleKeydown(event) {
+    if(event.key === 'Escape') {
+        Modal.close()
+    }
+}
+
+
+
 
